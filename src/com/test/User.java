555ee472f014e05ae8f5ec222eae012c3e66f299
grop7 <grop7@naver.com>
2023-08-30 14:43:58 +0900

    2nd commit

--- src/com/test/User.java
+++ src/com/test/User.java
@@ -5,7 +5,10 @@ public class User {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
         
     }
 
